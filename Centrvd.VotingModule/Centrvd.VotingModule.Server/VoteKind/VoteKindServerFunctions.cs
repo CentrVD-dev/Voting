be4c89b9eaee3c9a3a04dc76585adbc3ee08ad0b
v.kostukevich <v.kostukevich@cantrvd.ru>
2025-09-05 14:37:00 +0300

    Справочники виды голосов, матрицы голосования. Собятия в задании на подготовку.

---
+++ Centrvd.VotingModule/Centrvd.VotingModule.Server/VoteKind/VoteKindServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Centrvd.VotingModule.VoteKind;
+
+namespace Centrvd.VotingModule.Server
+{
+  partial class VoteKindFunctions
+  {
+
+  }
+}
--- Centrvd.VotingModule/Centrvd.VotingModule.Shared/VotingPrepareAssignment/VotingPrepareAssignmentHandlers.cs
+++ Centrvd.VotingModule/Centrvd.VotingModule.Shared/VotingPrepareAssignment/VotingPrepareAssignmentHandlers.cs
@@ -7,8 +7,48 @@ using Centrvd.VotingModule.VotingPrepareAssignment;
 
 namespace Centrvd.VotingModule
 {
-  partial class VotingPrepareAssignmentSharedHandlers
+  partial class VotingPrepareAssignmentVotingPointsSharedHandlers
   {
 
+    public virtual void VotingPointsVoteMatrixChanged(Centrvd.VotingModule.Shared.VotingPrepareAssignmentVotingPointsVoteMatrixChangedEventArgs e)
+    {
+      if (Equals(e.NewValue, e.OldValue))
+        return;
+
+      // Обновляем матрицы голосования для всех точек, если требуется
+      if (_obj.VotingPrepareAssignment.SameMatrixForAllPoints == true)
+      {
+        foreach (var votingPoint in _obj.VotingPrepareAssignment.VotingPoints)
+          votingPoint.VoteMatrix = e.NewValue;
+      }
+
+      // Обновляем виды голосов
+      _obj.VoteKinds = e.NewValue != null
+        ? string.Join(", ", e.NewValue.Variants.Select(v => v.VoteKind.Name))
+        : null;
+    }
+  }
+
+  partial class VotingPrepareAssignmentVotingPointsSharedCollectionHandlers
+  {
+
+    public virtual void VotingPointsDeleted(Sungero.Domain.Shared.CollectionPropertyDeletedEventArgs e)
+    {
+      foreach (var item in _obj.VotingPoints.Where(p => p.Number > _deleted.Number))
+        item.Number--;
+    }
+
+    public virtual void VotingPointsAdded(Sungero.Domain.Shared.CollectionPropertyAddedEventArgs e)
+    {
+      _added.Number = (_obj.VotingPoints.Max(a => a.Number) ?? 0) + 1;
+      
+      if (_obj.SameMatrixForAllPoints == true && _obj.VotingPoints.Any())
+      {
+        var pointWithMatrix = _obj.VotingPoints.FirstOrDefault(p => p.VoteMatrix != null);
+        if (pointWithMatrix != null)
+          _added.VoteMatrix = pointWithMatrix.VoteMatrix;
+      }
+      
+    }
   }
 }
